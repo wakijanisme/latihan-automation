a81fe77e71d6a861b0ddddb6e1669f62554b9ff2
wakijanisme <ahmadsyaughi@gmail.com>
2024-05-11 21:00:55 +0700

    [Nomor Tiket] pertemuan 1 (skenario login) v.1

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -3,16 +3,17 @@ Given('I am on the homepage automationexercise.com') do
 end
 
 When('I click sign in on the homepage') do
-  pending
-  #find(xpath, "//a[@href='/login']").click
-  #expect(URI.parse(current_url)).to have_content("/login")
-  #find
+  find(:xpath, "//a[@href='/login']").click
+  expect(URI.parse(current_url)).to have_content("/login")
+  find(:xpath, "//h2[text()='Login to your account']").should be_visible
 end
 
 When('I fill my credentials on the homepage') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find(:xpath, "//input[@data-qa='login-email']").set("ogitest@gmail.com")
+  fill_in 'password', with: 'Ogitest123'
+  click_on "Login"
 end
 
 Then('I should be logged in') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find(:xpath, "//*[text()='Ogi Test']").should be_visible
 end
